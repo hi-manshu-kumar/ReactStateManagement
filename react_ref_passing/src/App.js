a4b950da70bcdcac48aa9b31569ca865c6c028d3
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-07-15 00:07:52 +0530

    Updated app.js that use refs to change and print value

--- react_ref_passing/src/App.js
+++ react_ref_passing/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      price : 0,
+      quantity : 0,
+      result : 0
+    };
   }
-}
 
-export default App;
+  computeIt(){
+    console.log(this.refs.price.value);
+    console.log(this.refs.quantity.value);
+    // console.log(this.refs.result.value);
+    this.setState({
+      ...this.state , result: parseInt(this.refs.price.value) * parseInt(this.refs.quantity.value)
+    });
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <input type="text" ref='price' placeholder = 'Enter price here' />
+        <input type="text" ref='quantity' placeholder= 'Enter quantity here'/>
+
+        <button onClick = {()=> this.computeIt()}>Compute it</button>
+        {this.state.result}
+      </div>
+    )
+  }
+} 
